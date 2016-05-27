9e5c9a9fdd98ca1ca1aa41df60d6ffb6955ae693
songbob <sky-bridge_@nate.com>
2016-05-27 16:23:15 +0900

    ejs + count <- 여러가지 views 합치기

--- app.js
+++ app.js
@@ -23,8 +23,22 @@ var path = require('path');
 app.set("view engine", 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
+var data={count:0};
 app.get('/', function(req,res){
-	res.render('my_first_ejs');
+	data.count++;
+	res.render('my_first_ejs', data);
+});
+app.get('/reset', function(req,res){
+	data.count=0;
+	res.render('my_first_ejs', data);
+});
+app.get('/set/count', function(req,res){
+	if(req.query.count) data.count=req.query.count;
+	res.render('my_first_ejs', data);
+});
+app.get('/set/:num', function(req,res){
+	data.count=req.params.num;
+	res.render('my_first_ejs', data);
 });
 
 app.listen(3000, function(){
