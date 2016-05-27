ccba8b9eea5fbb2b2c05a3a8f1aa4472e8257f95
songbob <sky-bridge_@nate.com>
2016-05-27 15:08:13 +0900

    index add

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var express = require('express');
 
 var app = express();
 
-app.get('/', function(req, res){
+/*app.get('/', function(req, res){
 	res.send('Git Success?');
-});
+});*/
+
+app.use(express.static(__dirname + '/public'));
 
 app.listen(3000, function(){
 	console.log('Server On!');
