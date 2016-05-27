92c9d7b7d0b5ccbee615311e6f80e93ca7d880f9
songbob <sky-bridge_@nate.com>
2016-05-27 15:21:46 +0900

    css add

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ var app = express();
 	res.send('Git Success?');
 });*/
 
-app.use(express.static(__dirname + '/public'));
-consol.log(__dirname);
+/*app.use(express.static(__dirname + '/public'));
+console.log(__dirname);*/
+
+var path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+console.log(__dirname);
 
 app.listen(3000, function(){
 	console.log('Server On!');
