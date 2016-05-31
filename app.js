9b8615dfc5294241e995e2256ff12afc1dbdec97
songbob <sky-bridge_@nate.com>
2016-05-31 18:16:21 +0900

    게시판만들기 및 세팅

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
+// import modules
 var express = require('express');
 var app = express();
+var path = require('path');
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
 /*
  *  DB connect 부분(시스템_환경변수로 path설정전 연결)
  *  이렇게 할 때 github에 free로 공유되어 db를 남이 건드릴 수 있음
 var mongoose = require('mongoose');
 mongoose.connect("mongodb://master:*alpha6168@ds049945.mlab.com:49945/songbob");
 */
+
+// connect database
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 db.once("open", function(){
@@ -16,7 +22,9 @@ db.on("error", function(err){
 	console.log("DB ERROR : ", err);
 });
 
-var dataSchema = mongoose.Schema({
+/*
+ * 몽고db 컬럼만들기 & 컬럼이 없으면 만들어주기
+ * var dataSchema = mongoose.Schema({
 	name:String,
 	count:Number
 });
@@ -29,7 +37,7 @@ Data.findOne({name:"myData"}, function(err,data){
 				console.log("Counter initialized :", data);
 		});
 	}
-});
+});*/
 
 /*
  * index 없이 할때, 서버->클라이언트 루트찾아주기,
@@ -48,9 +56,35 @@ console.log(__dirname);*/
 app.use(express.static(path.join(__dirname, 'public')));
 console.log(__dirname);*/
 
-var path = require('path');
+//model setting
+var postSchema = mongoose.Schema({
+	title : {type:String, required:true},
+	body : {type:String, required:true},
+	createdAt : {type:Date, default:Date.now},
+	updatedAt : Date
+});
+var Post = mongoose.model('post', postSchema);
+
+//view setting
 app.set("view engine", 'ejs');
+
+//set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+
+//set routes
+app.get('/posts', function(req,res) {
+	Post.fin({}, function(err,posts){
+		if(err) return res.json({success:false, message:err});
+		res.json({success:true, data:posts});
+	});
+}); //index
+app.post('/posts', function(req,res){
+	Post.create(req.body.post, function(err,post){
+		if(err) return res.json({success:false, message:err});
+		res.json({success:true, data:post});
+	});
+}); //create
 
 /*
  * get방식으로 최상의 루트(/)로 접근시 req & res 설정
@@ -60,7 +94,26 @@ app.get('/', function(req,res){
 	res.render('my_first_ejs', data);
 });*/
 
-app.get('/', function(req,res){
+/*
+ * setCounter & getCounter 함수 사용하기 전 get방식으로 session에
+ * count 증가 및 초기화 저장하여 연산
+ * app.get('/reset', function(req,res){
+	data.count=0;
+	res.render('my_first_ejs', data);
+});
+app.get('/set/count', function(req,res){
+	if(req.query.count) data.count=req.query.count;
+	res.render('my_first_ejs', data);
+});
+app.get('/set/:num', function(req,res){
+	data.count=req.params.num;
+	res.render('my_first_ejs', data);
+});*/
+
+/*
+ * setCounter & getCounter 함수 사용하여 mongoDB로
+ * count 증가 및 초기화 저장하여 연산
+ * app.get('/', function(req,res){
 	Data.findOne({name:"myData"}, function(err,data){
 		if(err) return console.log("Data ERROR:",err);
 		data.count++;
@@ -99,24 +152,9 @@ function getCounter(res){
 		if(err) return console.log("Data ERROR:", err);
 		res.render('my_first_ejs', data);
 	});
-}
+}*/
 
-/*
- * setCounter & getCounter 함수 사용하기 전 get방식으로 session에
- * count 증가 및 초기화 저장하여 연산
- * app.get('/reset', function(req,res){
-	data.count=0;
-	res.render('my_first_ejs', data);
-});
-app.get('/set/count', function(req,res){
-	if(req.query.count) data.count=req.query.count;
-	res.render('my_first_ejs', data);
-});
-app.get('/set/:num', function(req,res){
-	data.count=req.params.num;
-	res.render('my_first_ejs', data);
-});*/
-
+//start server
 app.listen(3000, function(){
 	console.log('Server On!');
 });
