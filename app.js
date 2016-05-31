461570ffaafb4e9cedb6c21fe22c5cef383a6a05
songbob <sky-bridge_@nate.com>
2016-05-31 18:38:38 +0900

    CRUD Test

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(bodyParser.json());
 
 //set routes
 app.get('/posts', function(req,res) {
-	Post.fin({}, function(err,posts){
+	Post.find({}, function(err,posts){
 		if(err) return res.json({success:false, message:err});
 		res.json({success:true, data:posts});
 	});
@@ -85,6 +85,25 @@ app.post('/posts', function(req,res){
 		res.json({success:true, data:post});
 	});
 }); //create
+app.get('/posts/:id', function(req,res){
+	Post.findById(req.params.id, function(err,post){
+		if(err) return res.json({success:false, message:err});
+		res.json({success:true, data:post});
+	});
+}); //show
+app.put('/posts/:id', function(req,res){
+		req.body.post.updatedAt=Date.now();
+		Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
+		if(err) return res.json({success:false, message:err});
+		res.json({success:true, message:post._id+" updated"});
+	});
+}); //update
+app.delete('/posts/:id', function(req,res){
+	Post.findByIdAndRemove(req.params.id, function(err,post){
+		if(err) return res.json({success:false, message:err});
+		res.json({success:true, message:post._id+" deleted"});
+	});
+}); //destroy
 
 /*
  * get방식으로 최상의 루트(/)로 접근시 req & res 설정
