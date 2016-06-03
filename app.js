48ea8aee525d7a3de94ce36969a437e781b7ac29
songbob <sky-bridge_@nate.com>
2016-06-03 18:31:18 +0900

    My Board 2

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var app = express();
 var path = require('path');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 /*
  *  DB connect 부분(시스템_환경변수로 path설정전 연결)
@@ -71,6 +72,8 @@ app.set("view engine", 'ejs');
 //set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
 
 //set routes
 app.get('/posts', function(req,res) {
@@ -79,29 +82,42 @@ app.get('/posts', function(req,res) {
 		res.render("posts/index", {data:posts});
 	});
 }); //index
+app.get('/posts/new', function(req,res){
+	res.render("posts/new");
+}); //new
 app.post('/posts', function(req,res){
 	Post.create(req.body.post, function(err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, data:post});
+		/*res.json({success:true, data:post});*/
+		res.redirect('posts');
 	});
 }); //create
 app.get('/posts/:id', function(req,res){
 	Post.findById(req.params.id, function(err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, data:post});
+		/*res.json({success:true, data:post});*/
+		res.render("posts/show", {data:post});
 	});
 }); //show
+app.get('/posts/:id/edit', function(req,res){
+	Post.findById(req.params.id, function(err,post){
+		if(err) return res.json({success:false, message:err});
+		res.render("posts/edit", {data:post});
+	});
+}); //edit
 app.put('/posts/:id', function(req,res){
 		req.body.post.updatedAt=Date.now();
 		Post.findByIdAndUpdate(req.params.id, req.body.post, function(err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, message:post._id+" updated"});
+		/*res.json({success:true, message:post._id+" updated"});*/
+		res.redirect('/posts/'+req.params.id);
 	});
 }); //update
 app.delete('/posts/:id', function(req,res){
 	Post.findByIdAndRemove(req.params.id, function(err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, message:post._id+" deleted"});
+		/*res.json({success:true, message:post._id+" deleted"});*/
+		res.redirect('/posts');
 	});
 }); //destroy
 
