b659568d6d6a2cd1cb8e0bab84507c0097199136
songbob <sky-bridge_@nate.com>
2016-06-14 17:19:42 +0900

    posts & login Connect

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ console.log(__dirname);*/
 var postSchema = mongoose.Schema({
 	title : {type:String, required:true},
 	body : {type:String, required:true},
-	author : {type:mongoose.Schema.Type.ObjectId, ref:'user', required:true},
+	author : {type:mongoose.Schema.Types.ObjectId, ref:'user', required:true},
 	createdAt : {type:Date, default:Date.now},
 	updatedAt : Date
 });
