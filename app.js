aa227ca8f859d072c2ffb6525d83ca7f5714e54f
songbob <sky-bridge_@nate.com>
2016-06-14 19:25:07 +0900

    ~2

--- app.js
+++ app.js
@@ -250,10 +250,10 @@ app.post('/posts', isLoggedIn, function(req,res){
 	});
 }); //create
 app.get('/posts/:id', function(req,res){
-	Post.findById(req.params.id, function(err,post){
+	Post.findById(req.params.id).poppulate("author").exec(function(err,post){
 		if(err) return res.json({success:false, message:err});
 		/*res.json({success:true, data:post});*/
-		res.render("posts/show", {data:post});
+		res.render("posts/show", {data:post}, user:req.user);
 	});
 }); //show
 app.get('/posts/:id/edit', function(req,res){
